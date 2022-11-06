2687fb8277a3ad2c2c2ecb10205e632c82c5f23a
Hubertus13 <jubertusnova@gmail.com>
2022-11-06 20:22:29 +0700

    Innitial Commit UGD 89 Project 1

---
+++ app/src/main/java/com/example/ugd89_b_10916_project1/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.ugd89_b_10916_project1
+
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.*
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private var mCamera: Camera? = null
+    private var mCameraView: CameraView? = null
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
+    private var currentCameraId = Camera.CameraInfo.CAMERA_FACING_BACK
+    lateinit var sensorStatusTV: TextView
+
+
+
+
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    if (currentCameraId == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                        currentCameraId = Camera.CameraInfo.CAMERA_FACING_FRONT;
+                    } else {
+                        currentCameraId = Camera.CameraInfo.CAMERA_FACING_BACK;
+                    }
+                    if (mCameraView != null) {
+                        mCamera?.stopPreview();
+                    }
+                    mCamera?.release()
+                    try {
+                        mCamera = Camera.open(currentCameraId)
+                    } catch (e: Exception) {
+                        Log.d("Error", "Failed to get Camera" + e.message)
+
+                    }
+                    if (mCamera != null) {
+                        mCameraView = CameraView(applicationContext, mCamera!!)
+                        val cameraView = findViewById<View>(R.id.FLCamera) as FrameLayout
+                        cameraView.addView(mCameraView)
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            mCamera = Camera.open()
+        } catch (e: Exception) {
+            Log.d("Error", "Failed to get Camera" + e.message)
+        }
+        if (mCamera != null) {
+            mCameraView = CameraView(this, mCamera!!)
+            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
+            camera_view.addView(mCameraView)
+        }
+
+        sensorStatusTV = findViewById(R.id.idSensorStatus)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        if (proximitySensor == null) {
+
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
+            findViewById<View>(R.id.imgClose) as ImageButton
+        imageClose.setOnClickListener { view: View? -> System.exit(0) }
+    }
+}
